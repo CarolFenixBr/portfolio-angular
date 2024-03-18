b5dc46884c90712d2a94fcd3cbda161a8606884b
Caroline Vargas <carol_uchiha_27@hotmail.com>
2024-03-18 01:57:40 -0300

    upgrade big

---
+++ src/app/modules/portfolio/componenets/knowledge/knowledge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+
+}
